b2219c2295a1b3f4f2ff0a4418834b8b9c03413c
lixiuliang <xiuliang.li@gi-de.com>
2019-03-05 15:03:13 +0800

    Basic BottomNavigationview impl

--- bottomnavigationviewtest/src/main/java/com/exmaple/bottomnavigationviewtest/MainActivity.java
+++ bottomnavigationviewtest/src/main/java/com/exmaple/bottomnavigationviewtest/MainActivity.java
@@ -1,13 +1,101 @@
 package com.exmaple.bottomnavigationviewtest;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
+    private ViewPagerAdapter viewPagerAdapter;
+    private ViewPager viewPager;
+    private MenuItem menuItem;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+        initListener();
+        initData();
     }
+
+    private void initView() {
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        viewPager = findViewById(R.id.view_pager);
+    }
+
+    private void initData() {
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(viewPagerAdapter);
+        List<Fragment> list = new ArrayList<>();
+        list.add(TestFragment.newInstance("首页"));
+        list.add(TestFragment.newInstance("钱包"));
+        list.add(TestFragment.newInstance("卡片"));
+        list.add(TestFragment.newInstance("个人"));
+        list.add(TestFragment.newInstance("测试"));
+        viewPagerAdapter.setList(list);
+    }
+
+    private void initListener() {
+        viewPager.addOnPageChangeListener(mOnPageChangeListener);
+        bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    private ViewPager.OnPageChangeListener mOnPageChangeListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            if (menuItem != null) {
+                menuItem.setChecked(false);
+            } else {
+                bottomNavigationView.getMenu().getItem(0).setChecked(false);
+            }
+            menuItem = bottomNavigationView.getMenu().getItem(position);
+            menuItem.setChecked(true);
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            menuItem = item;
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    viewPager.setCurrentItem(0);
+                    return true;
+                case R.id.navigation_dashboard:
+                    viewPager.setCurrentItem(1);
+                    return true;
+                case R.id.navigation_notifications:
+                    viewPager.setCurrentItem(2);
+                    return true;
+                case R.id.navigation_person:
+                    viewPager.setCurrentItem(3);
+                    return true;
+                case R.id.navigation_person1:
+                    viewPager.setCurrentItem(4);
+                    return true;
+            }
+            return false;
+        }
+    };
 }
